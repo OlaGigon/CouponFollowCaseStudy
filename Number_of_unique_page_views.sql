7143bce5946e5b5038669ec74e0734a5d6e416be
Ola Gigon <44475277+OlaGigon@users.noreply.github.com>
2023-06-06 16:58:12 +0200

    Update Number_of_unique_page_views.sql

--- Number_of_unique_page_views.sql
+++ Number_of_unique_page_views.sql
@@ -1,2 +1,6 @@
-select count(distinct PV.EVENT_ID) as UniquePageViews
-from PageViews as PV
+SELECT COUNT(DISTINCT PV.EVENT_ID) AS UniquePageViews
+FROM PageViews AS PV
+
+/* Additional comments: 
+My concern here was the methodology of assigning EVENT_IDs. Are there changing after every time a given user refreshes the website? Or are there some time criteria after which there is a new event_id for the same user (30 minutes of inactivity etc.)
+*/
