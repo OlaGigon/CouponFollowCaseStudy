512162a7e5c7a721decf7b36c6314398e0577f84
Ola Gigon <44475277+OlaGigon@users.noreply.github.com>
2023-06-06 16:58:50 +0200

    Update Month_over_month_comparison_of_revenue_broken_down_by_affiliate_network.sql

--- Month_over_month_comparison_of_revenue_broken_down_by_affiliate_network.sql
+++ Month_over_month_comparison_of_revenue_broken_down_by_affiliate_network.sql
@@ -1,25 +1,30 @@
-with t1 as (select 
+WITH t1 AS (SELECT 
 AFFILIATENETWORKNAME
 , February
 , March
-
-from 
+FROM 
 (
-select SUM(COMMISSIONAMOUNT)/Count(Distinct Cast(TRANSACTIONDATE as Date)) as MonthlyRev, 
-CASE WHEN MONTH(TRANSACTIONDATE) =2 THEN 'February' else 'March' end as MonthName, 
+  --calculating daily average nnumber of orders per month for all of affiliate networks
+SELECT SUM(COMMISSIONAMOUNT)/COUNT(DISTINCT CAST(TRANSACTIONDATE AS Date)) AS MonthlyRev, 
+CASE WHEN MONTH(TRANSACTIONDATE) =2 THEN 'February' ELSE 'March' END AS MonthName, 
 AF.AFFILIATENETWORKNAME
-from Orders as O
-LEFT JOIN Affiliate_Network as AF on AF.AFFILIATENETWORKID = O.AFFILIATENETWORKID
-group by 2, 3
-) as t
-
+FROM Orders AS O
+LEFT JOIN Affiliate_Network AS AF ON AF.AFFILIATENETWORKID = O.AFFILIATENETWORKID
+GROUP BY 2, 3
+) AS t
+--creating a pivot table to compare between months
 PIVOT
 (
   SUM(MonthlyRev)
   FOR MonthName IN ('February', 'March' )
 )
 )
+SELECT *,
+CASE WHEN March>February THEN 'increase' ELSE 'decrease' END AS month_over_month
+FROM t1
 
-select *,
-CASE WHEN March>February then 'increase' else 'decrease' end as month_over_month
-from t1
+/* additional comments: 
+1. Data that you provided contains different number of days for February and March
+(less for March). In order to be able to compare between those two months I calculated daily averages for each month. Otherwise we'd probably see a decreasing trend in March for most of the afiliate networks. 
+2. Taking the above mentioned approach doesn't have to be 100% correct as we need to make sure that we take seasonality factors into consideration. I assume that as a web platform you generate more traffic (which results in higher number of orders) during weekends so it should be taken into consideration in month-over-month comparison. In this case and data that you sent we should make sure that there is a same share of weekends in both of the analyzed months.
+*/
