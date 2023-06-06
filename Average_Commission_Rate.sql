520dd54696cfc670bde506e832eae28ac49280a8
Ola Gigon <44475277+OlaGigon@users.noreply.github.com>
2023-06-06 16:57:53 +0200

    Update Average_Commission_Rate.sql

--- Average_Commission_Rate.sql
+++ Average_Commission_Rate.sql
@@ -1,2 +1,6 @@
-select ROUND(SUM(COMMISSIONAMOUNT)/SUM(SALEAMOUNT), 5) as CommisionRate 
-from Orders
+SELECT ROUND(SUM(COMMISSIONAMOUNT)/SUM(SALEAMOUNT), 5) AS CommisionRate 
+FROM Orders
+
+/* Additional comments: 
+1. I was thinking of using AVG(COMMISSIONAMOUNT/SALEAMOUNT) but in this case I wouldn't take weights of Sale Amount into consideration and as a consequence I wouldn't differentiate between them.
+*/
